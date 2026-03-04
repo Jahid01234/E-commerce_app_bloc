6f00bc91a4c2b1eeae042a8a2db1fd97844705cc
Jahid01234 <mdjahidhasan790523@gmail.com>
2026-03-04 11:29:57 +0600

    project structure setup......

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:bloc_ecommerce_app/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BlocEcommerceApp extends StatelessWidget {
+  const BlocEcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [],
+      child: MultiBlocProvider(
+        providers: [],
+        child: ScreenUtilInit(
+          designSize: const Size(360, 690),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (_, child) {
+            return MaterialApp.router(
+              theme: const MaterialTheme(TextTheme()).light(),
+              darkTheme: const MaterialTheme(TextTheme()).dark(),
+              debugShowCheckedModeBanner: false,
+              //routerConfig: RoutePages.ROUTER,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
